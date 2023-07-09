0dc8eda9fb4472092a8e5cff3495380af7c97a93
la55u <andras9405@gmail.com>
2023-07-09 19:35:09 +0200

    add minimal repro

--- apps/next/pages/index.tsx
+++ apps/next/pages/index.tsx
@@ -1,13 +1,30 @@
-import { HomeScreen } from 'app/features/home/screen'
-import Head from 'next/head'
+import { styled, XStack, Paragraph } from '@my/ui'
+
+const Frame = styled(XStack, {
+  px: '$3',
+  bg: 'gainsboro',
+  variants: {
+    active: { true: { bg: 'navy', borderColor: 'navy' } },
+  } as const,
+})
+
+const Label = styled(Paragraph, {
+  tag: 'span',
+  color: 'rebeccapurple',
+  variants: {
+    active: { true: { color: 'white' } },
+  } as const,
+})
 
 export default function Page() {
   return (
-    <>
-      <Head>
-        <title>Home</title>
-      </Head>
-      <HomeScreen />
-    </>
+    <XStack m={30} gap="$2">
+      <Frame active>
+        <Label active>Hello</Label>
+      </Frame>
+      <Frame>
+        <Label>Test</Label>
+      </Frame>
+    </XStack>
   )
 }
